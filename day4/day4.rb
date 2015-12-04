293c494652e58fd2dcef1cdc38ff36ada76d003e
HorizonShadow <joshleblanc94@gmail.com>
2015-12-04 10:06:08 -0400

    Day 4

---
+++ day4/day4.rb
@@ -0,0 +1,15 @@
+require 'digest'
+
+def get_hex(str, num)
+  Digest::MD5.hexdigest "#{str}#{num}"
+end
+
+def find_md5_hash_with_leading_zeroes(num_zeroes, str)
+  0.upto((10 ** str.length) - 1).each do |i|
+    return i if get_hex(str, i)[0...num_zeroes] == '0' * num_zeroes
+  end
+end
+
+input = File.read('input').strip
+p "The first hash with five leading zeroes comes from #{input}#{find_md5_hash_with_leading_zeroes 5, input}"
+p "The first hash with six leading zeroes comes from #{input}#{find_md5_hash_with_leading_zeroes 6, input}"
