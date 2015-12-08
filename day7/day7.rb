bb785fd56b3a081f44f4a6b049c63727e3dab979
HorizonShadow <joshleblanc94@gmail.com>
2015-12-08 14:19:09 -0400

    Removed usused code

--- day7/day7.rb
+++ day7/day7.rb
@@ -1,14 +1,6 @@
 require_relative 'circuit.rb'
 require_relative 'instruction.rb'
 
-def read_file_into_circuit circuit
-  File.open('input', 'r') do |infile|
-    while(line = infile.gets)
-      circuit.consume Instruction.new line.strip
-    end
-  end
-end
-
 circuit = Circuit.new
 circuit.consume_from_file 'input'
 circuit.process!
