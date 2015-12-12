52ceada3018484542e02ed5320269858eeace88a
Joshua LeBlanc <JoshLeBlanc94@gmail.com>
2015-12-12 11:45:04 -0400

    fixed tabs

--- day12/day12.rb
+++ day12/day12.rb
@@ -1,27 +1,26 @@
 require 'json'
-require 'pp'
 
 def get_sum_all_numbers(input)
   input.scan(/-?\d+/).map(&:to_i).inject(:+)
 end
 
 def find_red_obj(input)
-	red = []
-	case input
-	when Array
-		input.each do |i|
-			red << find_red_obj(i)
-		end
-	when Hash
-		if input.values.include? "red"
-			return input.to_s
-		else
-			input.values.each do |v|
-				red << find_red_obj(v)
-			end
-		end
-	end
-	return red.flatten
+  red = []
+  case input
+  when Array
+    input.each do |i|
+      red << find_red_obj(i)
+    end
+  when Hash
+    if input.values.include? "red"
+      return input.to_s
+    else
+      input.values.each do |v|
+        red << find_red_obj(v)
+      end
+    end
+  end
+  return red.flatten
 end
 
 
@@ -31,7 +30,7 @@ p "The sum of all numbers is #{sum_all_nums}"
 
 json = JSON.parse(input)
 sum_red = find_red_obj(json).inject(0) do |sum, red|
-	sum + get_sum_all_numbers(red)
+  sum + get_sum_all_numbers(red)
 end
 
 p "Sum with reds removed is: #{sum_all_nums - sum_red}"
