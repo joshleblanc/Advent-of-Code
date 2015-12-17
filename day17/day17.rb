a64c71f95162efd217f6bbcda31d4370b3f2ca99
HorizonShadow <joshleblanc94@gmail.com>
2015-12-17 09:16:26 -0400

    Fixed spacing

--- day17/day17.rb
+++ day17/day17.rb
@@ -1,4 +1,4 @@
-containers = File.read('input').lines.map{|n| n.to_i }
+containers = File.read('input').lines.map{ |n| n.to_i }
 combinations = containers.size.times.with_object([]) { |i, o| o << containers.combination(i).select { |a| a.inject(:+) == 150 } }.reject { |n| n.empty? }
 
 p "The number of different combinations is #{combinations.flatten(1).size}"
