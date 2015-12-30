9041d16f54a9bfde565413f05f5b87458a05dc95
HorizonShadow <joshleblanc94@gmail.com>
2015-12-30 11:42:17 -0400

    day 20

---
+++ day20/day20.rb
@@ -0,0 +1,34 @@
+require 'prime'
+
+# http://stackoverflow.com/a/3398195/2424975
+def divisors_of(number)
+  primes, powers = number.prime_division.transpose
+  exponents = powers.map{ |i| (0..i).to_a}
+  divisors = exponents.shift.product(*exponents).map do |powers|
+    primes.zip(powers).map{|prime, power| prime ** power}.inject(:*)
+  end
+  divisors
+end
+
+input = File.read('input').strip.to_i
+
+def find(num)
+  2.upto(num / 10).find do |i|
+    divisors_of(i).inject(:+) * 10 >= num
+  end
+end
+
+def find_with_lazy_elves(num)
+  times_delivered = []
+  2.upto(num / 10).find do |i|
+    divisors = divisors_of(i)
+    divisors.select do |div|
+      times_delivered[div] ||= -1
+      times_delivered[div] += 1
+      times_delivered[div] <= 50
+    end.inject(:+) * 11 >= num
+  end
+end
+
+p "The lowest house number to get at least #{input} presents is #{find(input)}"
+p "The lowest house number to get at least #{input} presents with lazy elves is #{find_with_lazy_elves(input)}"
