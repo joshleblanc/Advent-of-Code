9ad5c3f47a9139069fc3af9bb71d19707b5bf5cc
HorizonShadow <joshleblanc94@gmail.com>
2015-12-30 16:31:47 -0400

    refactored

--- day19/day19.rb
+++ day19/day19.rb
@@ -1,36 +1,28 @@
-def get_molecules(original_word, r)
-  tmp = original_word
-  matches = 0
-  original_word.chars.each_cons(r.first.length).with_object([]) do |w, obj|
+def replace(word, matches, replacement)
+  word.gsub(replacement.first).with_index do |m, i|
+    i == matches ? replacement.last : m
+  end
+end
+
+def get_molecules(word, r)
+  word.chars.each_cons(r.first.length).with_object([]) do |w, obj|
     next unless w.join == r.first
-    before = tmp
-    after = before.gsub(w.join).with_index do |m, i|
-      i == matches ? r.last : m
-    end
-    unless before == after
-      obj << after
-      matches += 1
-    end
+    after = replace(word, obj.length, r)
+    obj << after unless after == word
   end
 end
 
 def find_r(curr_str, curr_rep, replacements, word, steps)
   get_molecules(curr_str, curr_rep.reverse).each do |m|
     return steps if m == word
-    replacements.reverse!.each do |r|
-     val = find_r(m, r, replacements, word, steps + 1)
-     return val if val
-    end
+    val = replacements.reverse!.find { |r| find_r(m, r, replacements, word, steps + 1) }
+    return val if val
   end
   nil
 end
 
 def find(replacements, word)
-  vals = []
-  replacements.map do |r|
-    Thread.new { vals << find_r(word, r, replacements, 'e', 0) }
-  end.each(&:join)
-  vals.compact.min + 1
+  replacements.map { |r| find_r(word, r, replacements, 'e', 0) } + 1
 end
 
 def get_num_distinct_molecules(replacements, original_word)
@@ -44,8 +36,6 @@ end
 lines = File.read('input').lines
 original_word = lines.last.strip
 replacements = get_replacements(lines)
-
-
 
 p "#{get_num_distinct_molecules(replacements, original_word)} distinct molecules can be created"
 p "It takes #{find(replacements, original_word)} steps to create #{original_word}"
