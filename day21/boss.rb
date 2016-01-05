d66b422abae32d266265a4c0b0f57ac10d8a42c1
HorizonShadow <joshleblanc94@gmail.com>
2016-01-05 10:52:33 -0400

    removed print

--- day21/boss.rb
+++ day21/boss.rb
@@ -4,7 +4,6 @@ class Boss < Entity
 
   def initialize(file)
     attributes = File.read(file).lines.map { |s| s.split.last }
-    p attributes
     weapon = Equipment.new("BossWeapon", 0, attributes[1], 0)
     armor = Equipment.new("BossArmor", 0, 0, attributes[2])
     super(attributes[0], weapon, armor)
